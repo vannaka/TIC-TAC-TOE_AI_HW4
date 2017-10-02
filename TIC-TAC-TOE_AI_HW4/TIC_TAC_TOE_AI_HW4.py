0d48f0728a84ef2e771d57a368cbac2d55bd9cf5
Luke Mammen <lukemammen@centurytel.net>
2017-10-02 12:17:56 -0500

    Added printBorad and makeMove functions.

--- TIC-TAC-TOE_AI_HW4/TIC_TAC_TOE_AI_HW4.py
+++ TIC-TAC-TOE_AI_HW4/TIC_TAC_TOE_AI_HW4.py
@@ -4,12 +4,15 @@
 #
 # Last Edited on 10-1-17
 
-startState = [['','','','',''],
-              ['','','','',''],
-              ['','','','',''],
-              ['','','','',''],
-              ['','','','',''],
-              ['','','','','']]
+import os   # To clear screen
+
+             #  0   1   2   3   4
+startState = [['X',' ',' ',' ',' '],# 0
+              [' ','X',' ',' ',' '],# 1
+              [' ',' ',' ',' ',' '],# 2
+              [' ',' ',' ','X',' '],# 3
+              [' ',' ',' ',' ','O'],# 4
+              [' ',' ',' ',' ',' ']]# 5
 
 #--------------------------------------------------------------
 #
@@ -18,6 +21,10 @@ startState = [['','','','',''],
 #
 #--------------------------------------------------------------
 def main():
+    printBorad( startState )
+    res = makeMove( startState, 'O', 1, 1)
+    printBorad( startState )
+    print( res )
     pass
 # main()
 
@@ -48,6 +55,58 @@ def beginnerDecision( state ):
     pass
 
 # beginnerDecision()
+
+
+#--------------------------------------------------------------
+#
+#   makeMove( state, player, mov_x, mov_y )
+#       Checks if the space at x,y is empty and valid, then
+#       places 'player' char there.
+#
+#--------------------------------------------------------------
+def makeMove( state, player, mov_x, mov_y ):
+    # The x,y cordinates are outside the range of the board
+    if mov_y < 0 and mov_y >= state.len() and mov_x < 0 and mov_x >= state[0].len():
+        return False
+
+    # Check that the chosen space is empty
+    if state[mov_y][mov_x] == ' ':
+        state[mov_y][mov_x] = player
+        return True
+    else:
+        return False
+# makeMove()
+
+
+#--------------------------------------------------------------
+#
+#   printBorad( state )
+#       Display the board
+#
+#--------------------------------------------------------------
+def printBorad( state ):
+    # Locals
+    row = 0
+
+    _=os.system("cls")
+
+    # Print column numbers
+    print(" ", end='')
+    for x in range( 0, len( state[0] ) ):
+        print( " " + str(x), end='')
+    print("")
+
+    # Print board
+    for y in state:
+        # Print row number
+        print( str(row), end='' )
+        row += 1
+
+        # Print rows
+        for x in y:
+            print( "|" + x, end='')
+        print("|")
+# printBorad()
 
 # Call main function
 main()
