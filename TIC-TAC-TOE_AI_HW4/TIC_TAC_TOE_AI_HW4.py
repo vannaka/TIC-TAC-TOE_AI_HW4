331504a996a240d116fee9c1ea2dd2e8b30869ed
Luke Mammen <lukemammen@centurytel.net>
2017-10-02 20:20:10 -0500

    Added code to check for n tiles in a vertical orientation.

--- TIC-TAC-TOE_AI_HW4/TIC_TAC_TOE_AI_HW4.py
+++ TIC-TAC-TOE_AI_HW4/TIC_TAC_TOE_AI_HW4.py
@@ -7,12 +7,12 @@
 import os   # To clear screen
 
              #  0   1   2   3   4
-startState = [['X',' ',' ',' ',' '],# 0
-              [' ','X',' ',' ',' '],# 1
-              [' ',' ',' ',' ',' '],# 2
-              [' ',' ',' ','X',' '],# 3
-              [' ',' ',' ',' ','O'],# 4
-              [' ',' ',' ',' ',' ']]# 5
+startState = [['X',' ','X','X',' '],# 0
+              ['X',' ','O','X','X'],# 1
+              [' ','X','O',' ','X'],# 2
+              [' ','X',' ','X',' '],# 3
+              [' ','X',' ','X','O'],# 4
+              [' ',' ',' ',' ','O']]# 5
 
 #--------------------------------------------------------------
 #
@@ -22,9 +22,14 @@ startState = [['X',' ',' ',' ',' '],# 0
 #--------------------------------------------------------------
 def main():
     printBorad( startState )
-    res = makeMove( startState, 'O', 1, 1)
-    printBorad( startState )
-    print( res )
+
+    moves = []
+
+    # Check for 2 in a rows
+    checkCols( startState, 'O', 2, moves )
+
+    print( moves )
+
     pass
 # main()
 
@@ -52,10 +57,65 @@ class GameBoardState( object ):
 #
 #--------------------------------------------------------------
 def beginnerDecision( state ):
+    
     pass
-
 # beginnerDecision()
 
+
+#--------------------------------------------------------------
+#
+#   checkBoard( state, player )
+#       Return two lists of x,y coordinates marking the
+#       location of possible moves. The first list marks the
+#       empty space at the end of 2 in a rows. The second list,
+#       3 in a rows.
+#
+#--------------------------------------------------------------
+def checkBoard( board, player ):
+    pass
+# checkBoard()
+
+
+def checkCols( board, player, numInARow, moves ):
+    # Locals
+    count = 0
+
+    # Loop through each column
+    for col in range( 0, len(board[0]) ):
+        # Check column from top to bottom
+        count = 0
+        for row in range( 0, len(board) ):
+            if board[row][col] == player:
+                count += 1
+            elif board[row][col] == ' ' and count == numInARow:
+                addMove( moves, col, row)
+                count = 0
+            else:
+                count = 0
+
+        # Check column from bottom to top
+        count = 0
+        for row in range( len(board) - 1, -1, -1 ):
+            if board[row][col] == player:
+                count += 1
+            elif board[row][col] == ' ' and count == numInARow:
+                addMove( moves, col, row)
+                count = 0
+            else:
+                count = 0
+# checkCols()
+
+
+def checkRows( board, player, orig_x, orig_y, numInARow, moves ):
+    pass
+
+def checkDiags( board, player, orig_x, orig_y, numInARow, moves ):
+    pass
+
+def addMove( moves, mov_x, mov_y ):
+    if moves.count( [mov_x, mov_y] ) == 0:
+        moves.append( [mov_x, mov_y] )
+    
 
 #--------------------------------------------------------------
 #
@@ -88,6 +148,7 @@ def printBorad( state ):
     # Locals
     row = 0
 
+    # Clear screen of previous board
     _=os.system("cls")
 
     # Print column numbers
